68f3830503a831e3daf5311f8af373551c6c66fa
m-imperfect <owner@m-imperfect.net>
2024-05-09 04:44:20 +0300

    add multiple templates

---
+++ resource-generator-fivem/templates/esx/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "${resourceName}"
+description "${description}"
+author "${author}"
+version "${version}"
+
+dependency 'es_extended'
+
+shared_scripts {
+	'@es_extended/imports.lua',
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
